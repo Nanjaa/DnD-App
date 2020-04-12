35a8747e53b3acb76f49455806fbb9a1b863cef6
Nanjaa <stephanie.r.olfert@gmail.com>
2020-04-12 10:14:09 -0700

    Fixed a few things to fit best practices

--- campaigns.py
+++ campaigns.py
@@ -51,10 +51,10 @@ campaigns = [
                 'class': 5
             }
         ],
-        'startDate': datetime.datetime.now(),
+        'start_date': datetime.datetime.now(),
         'status': 1
     },
-{
+    {
         'id': 2,
         'name': 'Storm Kings Thunder 2',
         'players': [
@@ -73,21 +73,21 @@ campaigns = [
                 'class': 1
             }
         ],
-        'startDate': datetime.datetime.now(),
+        'start_date': datetime.datetime.now(),
         'status': 1
     }
 ]
 
 
 @app.route('/campaigns', methods=['GET'])
-def getCampaigns():
-    activeCampaigns = [campaign for campaign in campaigns if campaign['status'] != 2]
-    return jsonify({'campaign': activeCampaigns})
+def get_campaigns():
+    active_campaigns = [campaign for campaign in campaigns if campaign['status'] != 2]
+    return jsonify({'campaign': active_campaigns})
 
 
 @app.route('/campaigns/<int:campaignId>', methods=['GET'])
-def getCampaignById(campaignId):
-    campaign = [campaign for campaign in campaigns if campaign['id'] == campaignId and campaign['status'] != 2]
+def get_campaign_by_id(campaign_id):
+    campaign = [campaign for campaign in campaigns if campaign['id'] == campaign_id and campaign['status'] != 2]
     if len(campaign) == 0:
         abort(404)
 
@@ -95,12 +95,12 @@ def getCampaignById(campaignId):
 
 
 @app.route('/campaigns', methods=['POST'])
-def newCampaign():
+def new_campaign():
     campaign = {
         'id': campaigns[-1]['id'] + 1,
         'name': request.json.get('name', 'New Campaign'),
         'players': request.json.get('players', []),
-        'startDate': request.json.get('startDate', datetime.datetime.now()),
+        'start_date': request.json.get('start_date', datetime.datetime.now()),
         'status': 1
     }
     campaigns.append(campaign)
@@ -108,8 +108,8 @@ def newCampaign():
 
 
 @app.route('/campaigns/<int:campaignId>', methods=['PUT'])
-def updateCampaign(campaignId):
-    campaign = [campaign for campaign in campaigns if campaign['id'] == campaignId]
+def update_campaign(campaign_id):
+    campaign = [campaign for campaign in campaigns if campaign['id'] == campaign_id]
     if len(campaign) == 0:
         abort(404)
     if not request.json:
@@ -117,21 +117,21 @@ def updateCampaign(campaignId):
     # TODO: Add validators here for type
     campaign[0]['name'] = request.json.get('name', campaign[0]['name'])
     campaign[0]['players'] = request.json.get('players', campaign[0]['players'])
-    campaign[0]['startDate'] = request.json.get('startDate', campaign[0]['startDate'])
+    campaign[0]['start_date'] = request.json.get('start_date', campaign[0]['startDate'])
     campaign[0]['status'] = request.json.get('status', campaign[0]['status'])
 
     return jsonify({'campaign': campaign[0]})
 
 
 @app.route('/campaigns/<int:campaignId>', methods=['DELETE'])
-def removeCampaign(campaignId):
-    campaign = [campaign for campaign in campaigns if campaign['id'] == campaignId]
+def remove_campaign(campaign_id):
+    campaign = [campaign for campaign in campaigns if campaign['id'] == campaign_id]
     if len(campaign) == 0:
         abort(404)
 
     campaign[0]['status'] = 2
 
-    return jsonify({'successfullyDeleted:': campaignId})
+    return jsonify({'successfullyDeleted:': campaign_id})
 
 
 if __name__ == '__main__':
